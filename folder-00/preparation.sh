aaa574acbe30186fcfcc7944bf0fb280bd1a9bfd
pcarone <p.carone@desotech.it>
2024-01-03 18:02:55 +0100

    fix script poppist tvb

--- folder-00/preparation.sh
+++ folder-00/preparation.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-git clone --depth=1 https://github.com/desotech-it/DSK402-public.git -b 1.28-dev >> $LOGFILE 2>&1 
+git clone --depth=1 https://github.com/desotech-it/DSK402-public.git -b 1.28-dev >> /dev/null 2>&1 
 
-mv DSK402-public CKAD-material >> $LOGFILE 2>&1 
+mv DSK402-public CKAD-material >> /dev/null 2>&1 
 
-cd CKAD-material >> $LOGFILE 2>&1 
+cd CKAD-material >> /dev/null 2>&1 
 
 chmod +x cleanup.sh
 
-for q in {00..27} ; do chmod +x folder-"$q"/*.sh ; done >> $LOGFILE 2>&1 
+for q in {00..27} ; do chmod +x folder-"$q"/*.sh ; done >> /dev/null 2>&1 
