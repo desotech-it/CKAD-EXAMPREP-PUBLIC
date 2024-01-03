90d6feeeffb0a6855696ecb2f8f51a3f45dce113
pcarone <p.carone@desotech.it>
2024-01-03 14:48:50 +0100

    update scripts

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-for q in {01..27} ; do kind delete cluster -n question-"$q" ; done
+for q in {01..27} ; do kind delete cluster -n question-"$q" ; done >> $LOGFILE 2>&1 
 
-rm -f /home/student/.kube/config
+rm -f /home/student/.kube/config >> $LOGFILE 2>&1 
 
-for q in {01..27} ; do rm /question-"$q"/*.yaml ; done
+for q in {01..27} ; do rm question-"$q"/*.yaml ; done >> $LOGFILE 2>&1 
--- question-00/preparation.sh
+++ question-00/preparation.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-git clone --depth=1 https://github.com/desotech-it/DSK402-public.git
+git clone --depth=1 https://github.com/desotech-it/DSK402-public.git -b 1.28-dev >> $LOGFILE 2>&1 
 
 
-mv DSK402-public CKAD-material
+mv DSK402-public CKAD-material >> $LOGFILE 2>&1 
 
-cd CKAD-material
+cd CKAD-material >> $LOGFILE 2>&1 
+
+
+for q in {01..27} ; do chmod +x question-"$q"/*.sh ; done >> $LOGFILE 2>&1 
