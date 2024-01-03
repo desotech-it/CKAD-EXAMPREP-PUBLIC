6b2e19c5cede353e2d7b71c9728d1188299dd3eb
pcarone <p.carone@desotech.it>
2024-01-03 15:11:01 +0100

    update sctipts

---
+++ folder-00/preparation.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git clone --depth=1 https://github.com/desotech-it/DSK402-public.git -b 1.28-dev >> $LOGFILE 2>&1 
+
+
+mv DSK402-public CKAD-material >> $LOGFILE 2>&1 
+
+cd CKAD-material >> $LOGFILE 2>&1 
+
+
+for q in {01..27} ; do chmod +x question-"$q"/*.sh ; done >> $LOGFILE 2>&1 
+
+for q in {00..27} ; do mv question-"$q" folder-"$q"; done 
