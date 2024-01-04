98205ac99655f07df5a773c7e791acd785b85fcb
Evangelista Tragni <93332785+etragni@users.noreply.github.com>
2024-01-04 16:24:38 +0100

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+
+export LOGFILE=clean$question.log
+touch $LOGFILE >> $LOGFILE 2>&1
+
 for q in {01..27} ; do kind delete cluster -n question-"$q" ; done >> $LOGFILE 2>&1 
 
 rm -f /home/student/.kube/config >> $LOGFILE 2>&1 
 
-for q in {01..27} ; do rm question-"$q"/*.yaml ; done >> $LOGFILE 2>&1 
+for q in {01..27} ; do rm folder-"$q"/*.yaml ; done >> $LOGFILE 2>&1 
